aa954d98ea4587f9e4e33ac2b76c3b097ead0592
Jan Kaluza <hanzz.k@gmail.com>
2011-07-27 16:54:22 +0200

    auth parsing

--- server/responders/login.cpp
+++ server/responders/login.cpp
@@ -28,6 +28,62 @@
 namespace HamLog {
 namespace Responder {
 	
+/* Inspiration taken from libgsasl */
+static void digest_md5_parse(std::map<std::string, std::string> &ret, const char *digest) {
+	const char *token_start, *val_start, *val_end, *cur;
+
+	cur = digest;
+	while(*cur != '\0') {
+		/* Find the end of the token */
+		int in_quotes = 0;
+		char *name, *value = NULL;
+		token_start = cur;
+		while(*cur != '\0' && (in_quotes || (!in_quotes && *cur != ','))) {
+			if (*cur == '"')
+				in_quotes = !in_quotes;
+			cur++;
+		}
+
+		/* Find start of value.  */
+		val_start = strchr(token_start, '=');
+		if (val_start == NULL || val_start > cur)
+			val_start = cur;
+
+		if (token_start != val_start) {
+			name = strndup(token_start, val_start - token_start);
+
+			if (val_start != cur) {
+				val_start++;
+				while (val_start != cur && (*val_start == ' ' || *val_start == '\t'
+						|| *val_start == '\r' || *val_start == '\n'
+						|| *val_start == '"'))
+					val_start++;
+
+				val_end = cur;
+				while (val_end >= val_start && (*val_end == ' ' || *val_end == ',' || *val_end == '\t'
+						|| *val_end == '\r' || *val_end == '\n'
+						|| *val_end == '"'  || *val_end == '\0'))
+					val_end--;
+
+				if (val_end - val_start + 1 >= 0)
+					value = strndup(val_start, val_end - val_start + 1);
+			}
+			ret[name] = value;
+			free(name);
+			free(value);
+		}
+
+		/* Find the start of the next token, if there is one */
+		if (*cur != '\0') {
+			cur++;
+			while (*cur == ' ' || *cur == ',' || *cur == '\t'
+					|| *cur == '\r' || *cur == '\n')
+				cur++;
+		}
+	}
+
+}
+	
 Login::Login() : RequestResponder("/login", false) {
 	
 }
@@ -53,7 +109,20 @@ bool Login::handleRequest(Session *session, Request::ref request, Reply::ref rep
 	}
 	else {
 		StorageBackend::User user = StorageBackend::getInstance()->getUser("test");
-		std::string auth = reply->getHeader("Authorization");
+		std::string auth = request->getHeader("Authorization");
+		std::map<std::string, std::string> fields;
+		digest_md5_parse(fields, auth.substr(7).c_str());
+
+#define HAS(NAME) (fields.find(NAME) != fields.end())
+
+		if (!HAS("username") || !HAS("realm") || !HAS("nonce") || !HAS("uri") ||
+			!HAS("qop") || !HAS("response") || !HAS("opaque")) {
+			createAuthorizationRequest(reply);
+			return true;
+		}
+
+#undef HAS
+
 		session->setAuthenticated(true);
 		reply->setContent("Authorized");
 		reply->setContentType("text/html");
