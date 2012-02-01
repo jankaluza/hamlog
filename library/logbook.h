69b96fe4bb510ec4229041ac6e25e3c91beb5f22
Jan Kaluza <hanzz.k@gmail.com>
2012-02-01 13:15:38 +0100

    docs

--- library/logbook.h
+++ library/logbook.h
@@ -1,8 +1,3 @@
-/**
- * @file logbook.h LogBook API
- * @ingroup core
- */
-
 /*
  * Hamlog
  *
@@ -36,11 +31,80 @@
 extern "C" {
 #endif
 
+/**
+ * @file logbook.h Logbook API
+ * @ingroup core
+ * @details Logbook API provides basic way how maintain the logbook.
+ * It allows to fetch the logbook and add or remove items from it.
+ * 
+ * Check the \ref signals "Signals page" to see signals provided by
+ * this module.
+ */
+
+/**
+ * Signal emitted when records are fetched.
+ * Data passed by this signal are in CSV format where the first row is header.
+ * Read \ref csv "Hamlog CSV description" for more information.
+ * \ingroup signals
+ */
+#define ham_signal_logbook_fetched "logbook-fetched"
+
+/**
+ * Signal emitted when the record is added/updated.
+ * \ingroup signals
+ */
+#define ham_signal_logbook_added "logbook-added"
+
+/**
+ * Signal emitted when the record is removed.
+ * \ingroup signals
+ */
+#define ham_signal_logbook_removed "logbook-removed"
+
+
+/**
+ * Fetches the logbook from the server.
+ * @param connection Connection associated with the request.
+ * @param handler Handler called when the data is fetched, or NULL.
+ * For the CSV data passed to the handler, check the \ref ham_signal_logbook_fetched signal description.
+ * @param ui_data Pointer to data passed to handler or NULL.
+ * @see ham_logbook_fetch_with_call()
+ * @see ham_signal_logbook_fetched
+ */
 void ham_logbook_fetch(HAMConnection *connection, HAMFetchHandler handler, void *ui_data);
 
+/**
+ * Fetches only the records with particular Call from the server.
+ * @param connection Connection associated with the request.
+ * @param call Call name to be used as filter.
+ * @param handler Handler called when the data is fetched, or NULL.
+ * For the CSV data passed to the handler, check the \ref ham_signal_logbook_fetched signal description.
+ * @param ui_data Pointer to data passed to handler or NULL.
+ * @see ham_logbook_fetch()
+ * @see ham_signal_logbook_fetched
+ */
 void ham_logbook_fetch_with_call(HAMConnection *connection, const char *call, HAMFetchHandler handler, void *ui_data);
 
+/**
+ * Adds or updates the record.
+ * @param connection Connection associated with the request.
+ * @param data CSV data representing the record.
+ * @param handler Handler called when the record is added/updated, or NULL.
+ * @param ui_data Pointer to data passed to handler or NULL.
+ * @see ham_logbook_fetch()
+ * @see ham_signal_logbook_added
+ */
 void ham_logbook_add(HAMConnection *connection, const char *data, HAMFetchHandler handler, void *ui_data);
+
+/**
+ * Removes the record.
+ * @param connection Connection associated with the request.
+ * @param data CSV data representing the record.
+ * @param handler Handler called when the record is removed, or NULL.
+ * @param ui_data Pointer to data passed to handler or NULL.
+ * @see ham_logbook_fetch()
+ * @see ham_signal_logbook_removed
+ */
 void ham_logbook_remove(HAMConnection *connection, const char *data, HAMFetchHandler handler, void *ui_data);
 
 void ham_logbook_register_signals();
--- library/signals.h
+++ library/signals.h
@@ -1,8 +1,3 @@
-/**
- * @file connection.h Connection API
- * @ingroup core
- */
-
 /*
  * Hamlog
  *
@@ -37,12 +32,76 @@
 extern "C" {
 #endif
 
+/**
+ * @file signals.h Signals API
+ * @ingroup core
+ * @details Signals API provides basic way how to be informed about Hamlog
+ * actions and incoming data.
+ * 
+ * From the UI developer perspective, you just
+ * have to register to the proper signal using ham_signals_register_handler()
+ * function. Once, the signal is emitted, your handler is called. Once, you
+ * decide you don't want to listen to this signal, unregister it using
+ * ham_signals_unregister_handler().
+ *
+ * Example:
+ * 
+ * @code
+ * static void handle_logged_in(HAMConnection *connection, const char *data, int error, void *ui_data) {
+ * 	if (!error)
+ * 		printf("We're logged in!\n");
+ * 	else
+ * 		printf("Not logged in: %s\n", data);
+ * 	}
+ * }
+ * 
+ * static void register_signals_handlers() {
+ * 	ham_signals_register_handler("account-logged-in", handle_logged_in, NULL);
+ * }
+ * 
+ * static void unregister_signals_handlers() {
+ *	ham_signals_unregister_handler("account-logged-in", handle_logged_in, NULL);
+ * }
+ * @endcode
+ * 
+ */
+
+/**
+ * Registers new signal.
+ * @param signal_name Name of the signal. For example "connection-connected".
+ * @see ham_signals_register_handler()
+ * @see ham_signals_emit_signal()
+ */
 void ham_signals_register_signal(const char *signal_name);
 
+
+/**
+ * Emits the signals. Calls all handlers registered to this signal.
+ * @param signal_name Name of the signal. For example "connection-connected".
+ * @param connection Connection associated with the signal.
+ * @param data Data passed to handler.
+ * @param error Zero if the signal does not inform about error state.
+ * @see ham_signals_register_handler()
+ * @see ham_signals_register_signal()
+ */
 void ham_signals_emit_signal(const char *signal_name, HAMConnection *connection, const char *data, int error);
 
+/**
+ * Registers a handler which is called when the signal is emitted.
+ * @param signal_name Name of the signal. For example "connection-connected".
+ * @param handler Handler which is called when the signal is emitted.
+ * @param ui_data Pointer to data object passed to handler to keep the context in the handler.
+ * @see ham_signals_unregister_handler()
+ */
 void ham_signals_register_handler(const char *signal_name, HAMFetchHandler handler, void *ui_data);
 
+/**
+ * Unregisters the handler.
+ * @param signal_name Name of the signal. For example "connection-connected".
+ * @param handler Handler to be unregistered.
+ * @param ui_data Pointer to data object passed to handler to keep the context in the handler.
+ * @see ham_signals_register_handler()
+ */
 void ham_signals_unregister_handler(const char *signal_name, HAMFetchHandler handler, void *ui_data);
 
 #ifdef __cplusplus
